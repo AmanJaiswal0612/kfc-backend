8927baa37c13ee99ee37f5f6f9d060e72fde67e0
AmanJaiswal0612 <aman.jaiswal0612@gmail.com>
2022-06-14 19:26:04 +0530

    done

---
+++ index.js
@@ -0,0 +1,40 @@
+const express= require("express");
+const {Menu,connection} = require("./db.js");
+
+
+
+const app= express();
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+
+
+app.get("/",(req,res)=>{
+    res.send("server started")
+})
+
+
+
+
+app.get("/item", async(req,res)=>{
+    let query= req.query;
+    
+    let items=await Menu.find({title:{$regex:req.query.search,$options:/i/}})
+    res.json(items)
+})
+
+app.get("/menu", async (req,res)=>{
+  let query= req.query;
+  let items= await Menu.find(query);
+  res.json(items);
+})
+
+const PORT =process.env.PORT||8080
+app.listen(PORT, async ()=>{
+    try{
+        await connection
+    }
+    catch(e){
+      console.log(e);
+    }
+})
